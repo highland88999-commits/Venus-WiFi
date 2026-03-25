f6d3f1726139327169721df77a316d6ab1388a08
Merlin Ambros <highland88999@gmail.com>
2026-03-24 19:41:23 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,36 +1,23 @@
-const CACHE_NAME = 'yeg-cyber-scout-v1';
-const ASSETS_TO_CACHE = [
+const CACHE_NAME = 'artemis-core-v2';
+const ASSETS = [
+  './',
   './index.html',
   './manifest.json',
   'https://unpkg.com/leaflet/dist/leaflet.css',
-  'https://unpkg.com/leaflet/dist/leaflet.js'
+  'https://unpkg.com/leaflet/dist/leaflet.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/PapaParse/5.3.2/papaparse.min.js'
 ];
 
-// Install Event: Cache core files
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS_TO_CACHE);
-    })
+// Install Assets
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
-// Activate Event: Cleanup old caches
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
-      );
-    })
-  );
-});
-
-// Fetch Event: Serve from cache if offline
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+// Intercept requests for Offline usage
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
   );
 });
